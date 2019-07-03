c432b0d9ec2b5d78c3b43aeaab3839c4590fbfc8
elkehinze <ehinze@pmpmg.com>
2019-07-03 14:50:58 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Sassy Bootstrap Boilerplate
+
+Lean Bootstrap 4 and Sass Boilerplate jump start any web project. 
+Includes:
+
+Dependencies:
+- Bootstrap 4
+- Webslide Menu
+- Slick Carousel
+- jQuery
+
+Dev Dependencies:
+- Node-Sass
+- Live reloading with BrowserSync
+- Imagemin WebP converter
---
+++ index.js
@@ -0,0 +1,15 @@
+//bs.js
+var bs = require('browser-sync').create();
+bs.init({
+    server: "dist",
+    files: ["dist/assets/styles/*.css"],
+    notify: false,
+    plugins: [
+        {
+            module: "bs-html-injector",
+            options: {
+                files: ["dist/*.html"]
+            }
+        }
+    ]
+});
